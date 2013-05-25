7ef9a04675bd7a523ed53dcc7eb836d4e908923e
William Dibbern <gotdibbs@server.fake>
2013-05-24 22:18:26 -0500

    Added more unit tests.

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,7 @@ exports.nodeunit = (function (){
     'use strict';
     
     var grunt = require('grunt'),
+        util = require('util'),
         _ = grunt.util._,
         QUnitMinimalist = require('../tasks/lib/QUnitMinimalist');
 
@@ -9,9 +10,10 @@ exports.nodeunit = (function (){
         if (!opts) { opts = {}; }
     
         return {
-            options: function options(extensions){ 
-                return _.extend(opts, extensions);
-            }
+            options: function options(){ 
+                return opts;
+            },
+            async: function noop() { }
         };
     }
     
@@ -24,12 +26,9 @@ exports.nodeunit = (function (){
     function setsDefaults(test) {
         var task = new QUnitMinimalist(_getMockTask());
         
-        test.expect(5);
+        test.expect(2);
         test.ok(task, 'Task should exist.');
         test.ok(task.options, 'Options should exist.');
-        test.ok(task.options.phantomOptions, 'Phantom Options should exist.');
-        test.strictEqual(task.options.page, null, 'Page should be null.');
-        test.strictEqual(task.options.parameters, null, 'Host parameters should default to null.');
         test.done();
     }
     
@@ -50,10 +49,62 @@ exports.nodeunit = (function (){
         test.ok(calledFunc, 'Should be passed a valid function.');
         test.done();
     }
+
+    function runRegistersPhantomHandlers(test) {
+        var registeredHandlers = [],
+            task,
+            mockPhantom = {
+                spawn: function noop() { },
+                on: function on(handler) { 
+                    registeredHandlers.push(handler)
+                }
+            };
+
+        task = new QUnitMinimalist(_getMockTask({ 
+            page: 'test/test.html', 
+            phantomOptions: { } 
+        }), mockPhantom);
+
+        test.expect(5);
+        task.run();
+        test.strictEqual(registeredHandlers.length, 4, 'Four handlers should be registered.');
+        test.notEqual(registeredHandlers.indexOf('qunit.done'), -1, 'qunit.done should be registered.');
+        test.notEqual(registeredHandlers.indexOf('qunit.log'), -1, 'qunit.log should be registered.');
+        test.notEqual(registeredHandlers.indexOf('fail.load'), -1, 'fail.load should be regisered.');
+        test.notEqual(registeredHandlers.indexOf('fail.timeout'), -1, 'fail.timeout should be registered.');
+        test.done();
+    }
+
+    function runSetsDefaults(test) {
+        var calledUrl,
+            calledOptions,
+            task,
+            mockPhantom = {
+                spawn: function spawn(url, options) {
+                    calledUrl = url;
+                    calledOptions = options;
+                },
+                on: function noop() { }
+            };
+
+        task = new QUnitMinimalist(_getMockTask({ 
+            page: 'test/test.html', 
+            phantomOptions: { } 
+        }), mockPhantom);
+
+        test.expect(3);
+        task.run();
+        test.strictEqual(calledUrl, 'test/test.html', 'Called URL should match what was passed in.');
+        test.ok(calledOptions, 'Called options should be defined.');
+        test.ok(calledOptions.options.inject.match(/qunit-bridge\.js$/), 'Inject should be defaulted to the default bridge.');
+        test.done();
+    }
     
     return {
         exists: exists,
         setsDefaults: setsDefaults,
-        registersSelfWithGrunt: registersSelfWithGrunt
+        registersSelfWithGrunt: registersSelfWithGrunt,
+        runRegistersPhantomHandlers: runRegistersPhantomHandlers,
+        runSetsDefaults: runSetsDefaults
     };
 }());
